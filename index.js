52ba724e85b29530b0728f4239938a1d7de1b04d
wang long <wanglong@uino.com>
2022-08-08 17:10:45 +0800

    chore: promise recap

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
+function ready(){
+    return new Promise((resolve, reject)=>{
+        navigator.serviceWorker.register("./sw.js").then(function(registration) {
+            // registration worked
+            console.log('Registration succeeded. Scope is ' + registration.scope);
+
+            if (registration.active) {
+                resolve()
+            }else if(registration.installing){
+                registration.installing.addEventListener('statechange', function (ev) {
+                    if (ev.target.state === 'activated') {
+                        resolve()
+                    }
+                });
+            }else if(registration.waiting){
+                // update situation ignored
+            }
+
+
+            navigator.serviceWorker.addEventListener('message', function (e) {
+                console.log('main thread got message:',e)
+            })
+
+            // registration.addEventListener('updatefound', function() {
+            //     // If updatefound is fired, it means that there's
+            //     // a new service worker being installed.
+            //     const installingWorker = registration.installing;
+            //     console.log('A new service worker is being installed:',
+            //         installingWorker);
+            //
+            //     installingWorker.onstatechange = function () {
+            //         console.log('new service worker onstatechange', installingWorker.state)
+            //     }
+            //
+            //     // You can listen for changes to the installing service worker's
+            //     // state via installingWorker.onstatechange
+            // });
+
+        }).catch(function(error) {
+            // registration failed
+            console.log('Registration failed with ' + error);
+        });
+    })
+}
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register("./sw.js").then(function(registration) {
         // registration worked
