9a75fe24d6dd831a277921c67eb17c4ae10d5f2b
wang long <wanglong@uino.com>
2022-08-11 09:21:00 +0800

    feat: sync xmlhttprequest

--- index.js
+++ index.js
@@ -1,110 +1,60 @@
-function ready(){
-    return new Promise((resolve, reject)=>{
-        navigator.serviceWorker.register("./sw.js").then(function(registration) {
+function ready() {
+    return new Promise((resolve, reject) => {
+        navigator.serviceWorker.register("./sw.js").then(function (registration) {
             // registration worked
             console.log('Registration succeeded. Scope is ' + registration.scope);
 
             if (registration.active) {
                 resolve()
-            }else if(registration.installing){
+            } else if (registration.installing) {
                 registration.installing.addEventListener('statechange', function (ev) {
                     if (ev.target.state === 'activated') {
                         resolve()
                     }
                 });
-            }else if(registration.waiting){
+            } else if (registration.waiting) {
                 // update situation ignored
             }
 
 
             navigator.serviceWorker.addEventListener('message', function (e) {
-                console.log('main thread got message:',e)
+                console.log('main thread got message:', e)
             })
 
-            // registration.addEventListener('updatefound', function() {
-            //     // If updatefound is fired, it means that there's
-            //     // a new service worker being installed.
-            //     const installingWorker = registration.installing;
-            //     console.log('A new service worker is being installed:',
-            //         installingWorker);
-            //
-            //     installingWorker.onstatechange = function () {
-            //         console.log('new service worker onstatechange', installingWorker.state)
-            //     }
-            //
-            //     // You can listen for changes to the installing service worker's
-            //     // state via installingWorker.onstatechange
-            // });
-
-        }).catch(function(error) {
+        }).catch(function (error) {
             // registration failed
             console.log('Registration failed with ' + error);
         });
     })
 }
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("./sw.js").then(function(registration) {
-        // registration worked
-        console.log('Registration succeeded. Scope is ' + registration.scope);
+ready().then(() => {
+    console.error('ready!!')
+})
 
-        console.log(registration.active ,registration.installing, registration.waiting )
-        if (registration.active) {
-            //loadSandbox();
-            navigator.serviceWorker.controller.postMessage('serviceWorker register succeed!!!')
-        }else if(registration.installing){
-            registration.installing.addEventListener('statechange', function (ev) {
-                if (ev.target.state === 'activated') {
-                    navigator.serviceWorker.controller.postMessage('serviceWorker register succeed!!!')
-                    console.log('installing activated ready')
-                }
-            });
-        }else if(registration.waiting){
-            // update situation ignored
-        }
-
-
-        navigator.serviceWorker.addEventListener('message', function (e) {
-            console.log('main thread got message:',e)
-        })
-
-        // registration.addEventListener('updatefound', function() {
-        //     // If updatefound is fired, it means that there's
-        //     // a new service worker being installed.
-        //     const installingWorker = registration.installing;
-        //     console.log('A new service worker is being installed:',
-        //         installingWorker);
-        //
-        //     installingWorker.onstatechange = function () {
-        //         console.log('new service worker onstatechange', installingWorker.state)
-        //     }
-        //
-        //     // You can listen for changes to the installing service worker's
-        //     // state via installingWorker.onstatechange
-        // });
-
-    }).catch(function(error) {
-        // registration failed
-        console.log('Registration failed with ' + error);
-    });
-
-
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-        console.error('serviceWorker controllerchange!')
+function sandbox() {
+    return syncSendMessageToServiceWorker({
+        args: [1,3],
+        code: '(a,b)=>a+b '
     })
+}
 
-}else{
-    console.error('no serviceWorker')
+
+function syncSendMessageToServiceWorker(body) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', './sandbox-fake-sync-url.html', true);
+    xhr.send(JSON.stringify(body));
+    // look ma, i'm synchronous (•‿•)
+    return xhr.responseText;
 }
 
 function doFetch() {
-    fetch('./movies.json')
-        .then(response => response.json())
-        .then(data => console.log(data));
 
+    console.error(sandbox())
 }
 
 
 function sendMessage() {
-    navigator.serviceWorker.controller.postMessage('hey you')
+    fetch('./movies.json')
+    // navigator.serviceWorker.controller.postMessage('hey you')
 }
--- sw.js
+++ sw.js
@@ -1,33 +1,43 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     console.error('sw installed')
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     console.error('sw activate')
     self.clients.claim();
 });
 
+self.addEventListener('message', function (event) {
+    console.error('sw got message', event)
 
-self.addEventListener('message', function(event) {
-    console.error('sw got message',event)
-
-    // self.clients.matchAll().then(function(clients){
-    //     console.error(clients)
-    // });
     console.error(event.source.id)
-    self.clients.get(event.source.id).then(function(client) {
-        client.postMessage("copy ! :"+event.data);
+    self.clients.get(event.source.id).then(function (client) {
+        client.postMessage("copy ! :" + event.data);
     });
 });
 
 
+self.addEventListener('fetch', async function (event) {
+    const request = event.request
+    console.error('sw fetch :', event.request)
 
-self.addEventListener('fetch', function(event) {
-    console.error('sw fetch',event.request)
+    const responseBody = {}
 
-    if(event.request.url.endsWith('.json') || event.request.url.endsWith('.png') ){
-        event.respondWith(Promise.resolve(new Response(JSON.stringify({text:'Hello from your friendly neighbourhood service worker!'}))));
-    }else{
-        event.respondWith( fetch(event.request))
+    if (event.request.url.endsWith('sandbox-fake-sync-url.html')) {
+        try {
+            const {args, code} = await request.clone().json()
+
+            const handler = new Function('return ' + code)()
+            responseBody.result = handler(...args)
+        } catch (e) {
+            responseBody.error = e
+        }
+        event.respondWith(new Response(JSON.stringify(responseBody),{headers:{
+                'status' : 200,
+                'content-type': 'application/json',
+                'Cache-Control': 'no-store',
+            }}));
+    } else {
+        event.respondWith(fetch(event.request))
     }
 });
